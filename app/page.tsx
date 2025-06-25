27a7697510a9d06e20ad73306cc0bcf71c1fe860
Itai Biton <itaibiton@Itais-MacBook-Pro.local>
2025-06-25 21:43:15 +0300

    fix lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import Services from "@/components/Services";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+// import Services from "@/components/Services";
+// import Header from "@/components/Header";
+// import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
-import Cursor from "@/components/Cursor";
+// import Cursor from "@/components/Cursor";
 
 
 export default function Home() {
